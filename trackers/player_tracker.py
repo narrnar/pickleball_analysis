ea84a5679b171ecc423040a4045178beb8cf036e
narrnar <darensivamu@gmail.com>
2025-09-08 11:57:22 -0700

    Add Trackers

---
+++ trackers/player_tracker.py
@@ -0,0 +1,20 @@
+from ultras import YOLO
+
+class PlayerTracker:
+    def __init__(self, model_path):
+        self.model = YOLO(model_path)
+
+    def detect_frames(self, frame):
+        results = self.model.track(frame, persist = True)
+        id_name_dict = results.names
+
+        player_dict = {}
+        for box in results.boxes:
+            track_id = int(box.id.tolist()[0])
+            result = box.xyxy.tolist()[0]
+            object_cls_id = box.cls.tolist()[0]
+            object_cls_name = id_name_dict[object_cls_id]
+            if object_cls_name == "person":
+                player_dict[track_id] = result
+
+        return player_dict
