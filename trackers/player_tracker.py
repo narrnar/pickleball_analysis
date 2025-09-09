84cb4b009ab32d11cdf0bf81718dba1f17c8b755
narrnar <darensivamu@gmail.com>
2025-09-08 18:22:19 -0700

    Add bounding boxes to players

--- trackers/player_tracker.py
+++ trackers/player_tracker.py
@@ -1,4 +1,5 @@
 from ultras import YOLO
+import cv2
 
 class PlayerTracker:
     def __init__(self, model_path):
@@ -27,3 +28,15 @@ class PlayerTracker:
                 player_dict[track_id] = result
 
         return player_dict
+    
+    def draw_bboxes(self, video_frames, player_detections):
+        output_video_frames = []
+        for frame, player_dict in zip(video_frames, player_detections):
+            # Draw player bounding boxes
+            for track_id, bbox in player_dict.items():
+                x1, y1, x2, y2 = bbox
+                cv2.putText(frame, f"Player ID: {track_id}", (int(bbox[0], int(bbox[1] - 10))), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 2)
+            output_video_frames.append(frame)
+            
+        return output_video_frames
