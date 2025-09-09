3d44566e29850dd94b23c159efd671594642f4f6
narrnar <darensivamu@gmail.com>
2025-09-08 18:22:44 -0700

    Updates to main

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 # Import necessary modules
+from utils import (read_video, 
+                   save_video)
+from trackers import PlayerTracker
 
 def main():
     # Read Video
+    input_video_path = 'input_videos/input_video.mp4'
+    video_frames = read_video(input_video_path)
+
 
     # Detect Players and Ball
+    player_tracker = PlayerTracker(model_path='yolov8x.pt')
+    player_detections = player_tracker.detect_frames(video_frames)
 
     # Court Line Detector Model
 
@@ -15,8 +23,11 @@ def main():
 
     # Convert positions to mini court positions
 
-    # Draw Output
+
+    # --- Draw Output ---
+
     # -- Draw Player Bounding Boxes
+    output_video_frames = player_tracker.draw_bboxes(video_frames, player_detections)
 
     # -- Draw Court Keypoints
 
@@ -25,6 +36,9 @@ def main():
     # -- Draw Player Stats
 
     # -- Draw Frame Number on Top Left Corner
+
+
+    save_video(output_video_frames, 'output_videos/output_video.avi')
 
     if __name__ == "__main__":
         main()
