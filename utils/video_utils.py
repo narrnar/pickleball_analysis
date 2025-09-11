b604400a43e18747521fb0ab540ffafce9b1b398
narrnar <darensivamu@gmail.com>
2025-09-11 15:56:29 -0700

    Update Video Utils

--- utils/video_utils.py
+++ utils/video_utils.py
@@ -13,8 +13,13 @@ def read_video(video_path):
     return frames
 
 def save_video(output_video_frames, output_video_path):
-    fourcc = cv2.VideoWriter_fourcc(*'MPJG')
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
     out = cv2.VideoWriter(output_video_path, fourcc, 24, (output_video_frames[0].shape[1], output_video_frames[0].shape[0]))
+
+    # catch silent failures
+    if not out.isOpened():
+        raise RuntimeError(f"Failed to open VideoWriter for {output_video_path}")
+    
     for frame in output_video_frames:
         out.write(frame)
     out.release()
