11ba677d9b3ff95fb5f5e82e1e24d73b4e6f23c7
narrnar <darensivamu@gmail.com>
2025-09-15 20:43:53 -0700

    Updates to Court Keypoints

--- court_line_detector/court_line_detector.py
+++ court_line_detector/court_line_detector.py
@@ -38,7 +38,7 @@ class CourtLineDetector:
             x = int(keypoints[i])
             y = int(keypoints[i + 1])
 
-            cv2.putImage(image, str(i//2), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            cv2.putText(image, str(i//2), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
             cv2.circle(image, (x, y), 5, (0, 0, 255), -1)
         return image
     
