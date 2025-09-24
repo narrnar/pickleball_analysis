a6ce952d49dc98aa923ca281e57c202517f3dfe3
narrnar <darensivamu@gmail.com>
2025-09-23 20:34:43 -0700

    Output Video Working

--- court_line_detector/court_line_detector.py
+++ court_line_detector/court_line_detector.py
@@ -18,18 +18,18 @@ class CourtLineDetector:
 
     def predict(self, image):
         # Predict the first frame only
-        img_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image_tensor = self.transform(img_rgb).unsqueeze(0)
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image_tensor = self.transform(image_rgb).unsqueeze(0)
 
         with torch.no_grad():
             outputs = self.model(image_tensor)
 
         keypoints = outputs.squeeze().cpu().numpy()
-        original_h, original_w = img_rgb.shape[:2]
+        original_h, original_w = image.shape[:2]
 
         # Rescale keypoints to original image size
-        keypoints[::2] *= original_w/244.0
-        keypoints[1::2] *= original_h/244.0
+        keypoints[::2] *= original_w/224.0
+        keypoints[1::2] *= original_h/224.0
 
         return keypoints
     
