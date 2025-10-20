bf97a5b8f80250882750478801b3367dfe90430e
narrnar <darensivamu@gmail.com>
2025-10-20 15:39:45 -0700

    Attempt to fix ball tracking

--- trackers/ball_tracker.py
+++ trackers/ball_tracker.py
@@ -30,7 +30,7 @@ class BallTracker:
         df_ball_positions['mid_y'] = (df_ball_positions['y1'] + df_ball_positions['y2']) / 2
         df_ball_positions['mid_y_rolling_mean'] = df_ball_positions['mid_y'].rolling(window = 5, min_periods = 1, center = False).mean()
         df_ball_positions['delta_y'] = df_ball_positions['mid_y_rolling_mean'].diff()
-        minimum_change_frames_for_hit = 15 # Change sensitivity for hit detection
+        minimum_change_frames_for_hit = 18 # Change sensitivity for hit detection
         for i in range(1, len(df_ball_positions) - int(minimum_change_frames_for_hit * 1.2)):
             negative_position_change = df_ball_positions['delta_y'].iloc[i] > 0 and df_ball_positions['delta_y'].iloc[i + 1] < 0 
             positive_position_change = df_ball_positions['delta_y'].iloc[i] < 0 and df_ball_positions['delta_y'].iloc[i + 1] > 0
