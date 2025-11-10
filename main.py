33c720621990f3b8abf0f625458383d2f37c123e
narrnar <darensivamu@gmail.com>
2025-11-10 11:42:53 -0800

    Show Stats

--- main.py
+++ main.py
@@ -49,97 +49,87 @@ def main():
     print(f"Ball Shots detected at frames: {ball_shot_frames}")
 
     # Convert positions to mini court positions
-    # player_mini_court_detections, ball_mini_court_detections = mini_court.convert_bounding_boxes_to_mini_court_coordinates(player_detections,
-    #                                                                                                     ball_detections,
-    #                                                                                                     court_keypoints)
+    player_mini_court_detections, ball_mini_court_detections = mini_court.convert_bounding_boxes_to_mini_court_coordinates(player_detections,
+                                                                                                        ball_detections,
+                                                                                                        court_keypoints)
 
     # --- COMMENT OUT(WIP) ---
-    # # Player stats data
-    # player_stats_data = [{
-    #     'frame_num': 0,
-    #     # Players 1 - 4 (ID player 5 needs to be fixed)
-    #     'player_1_number_of_shots': 0,
-    #     'player_1_total_shot_speed': 0,
-    #     'player_1_last_shot_speed': 0,
-    #     'player_1_total_player_speed': 0,
-    #     'player_1_last_player_speed': 0,
+    # Player stats data
+    player_stats_data = [{
+        'frame_num': 0,
+        # Players 1 - 4 (ID player 5 needs to be fixed)
+        'player_1_number_of_shots': 0,
+        'player_1_total_shot_speed': 0,
+        'player_1_last_shot_speed': 0,
+        'player_1_total_player_speed': 0,
+        'player_1_last_player_speed': 0,
         
-    #     'player_2_number_of_shots': 0,
-    #     'player_2_total_shot_speed': 0,
-    #     'player_2_last_shot_speed': 0,
-    #     'player_2_total_player_speed': 0,
-    #     'player_2_last_player_speed': 0,
+        'player_2_number_of_shots': 0,
+        'player_2_total_shot_speed': 0,
+        'player_2_last_shot_speed': 0,
+        'player_2_total_player_speed': 0,
+        'player_2_last_player_speed': 0
+    }]
 
-    #     'player_3_number_of_shots': 0,
-    #     'player_3_total_shot_speed': 0,
-    #     'player_3_last_shot_speed': 0,
-    #     'player_3_total_player_speed': 0,
-    #     'player_3_last_player_speed': 0,
+    # Ball shot stats
+    for ball_shot_ind in range(len(ball_shot_frames)-1):
+        start_frame = ball_shot_frames[ball_shot_ind]
+        end_frame = ball_shot_frames[ball_shot_ind + 1]
+        ball_shot_time_in_seconds = (end_frame - start_frame) / 24
 
-    #     'player_4_number_of_shots': 0,
-    #     'player_4_total_shot_speed': 0,
-    #     'player_4_last_shot_speed': 0,
-    #     'player_4_total_player_speed': 0,
-    #     'player_4_last_player_speed': 0
-    # }]
+        # Get distance covered by the ball
+        distance_covered_by_ball_pixels = measure_distance(ball_mini_court_detections[start_frame][1],
+                                                           ball_mini_court_detections[end_frame][1])
+        distance_covered_by_ball_meters = convert_pixel_distance_to_meters(distance_covered_by_ball_pixels,
+                                                                           constants.COURT_WIDTH,
+                                                                           mini_court.get_width_of_mini_court()
+                                                                           )
 
-    # # Ball shot stats
-    # for ball_shot_ind in range(len(ball_shot_frames)-1):
-    #     start_frame = ball_shot_frames[ball_shot_ind]
-    #     end_frame = ball_shot_frames[ball_shot_ind + 1]
-    #     ball_shot_time_in_seconds = (end_frame - start_frame) / 24
+        # Speed of the ball shot in km/h
+        speed_of_ball_shot = distance_covered_by_ball_meters / ball_shot_time_in_seconds * 3.6
 
-    #     # Get distance covered by the ball
-    #     distance_covered_by_ball_pixels = measure_distance(ball_mini_court_detections[start_frame][1],
-    #                                                        ball_mini_court_detections[end_frame][1])
-    #     distance_covered_by_ball_meters = convert_pixel_distance_to_meters(distance_covered_by_ball_pixels,
-    #                                                                        constants.COURT_WIDTH,
-    #                                                                        mini_court.get_width_of_mini_court()
-    #                                                                        )
-
-    #     # Speed of the ball shot in km/h
-    #     speed_of_ball_shot = distance_covered_by_ball_meters / ball_shot_time_in_seconds * 3.6
-
-    #     # Player with the ball
-    #     player_positions = player_mini_court_detections[start_frame]
-    #     player_shot_ball = min(player_positions.keys(), key=lambda player_id: measure_distance(player_positions[player_id],
-    #                                                                                             ball_mini_court_detections[start_frame][1]))
+        # Player with the ball
+        player_positions = player_mini_court_detections[start_frame]
+        player_shot_ball = min(player_positions.keys(), key=lambda player_id: measure_distance(player_positions[player_id],
+                                                                                                ball_mini_court_detections[start_frame][1]))
         
-    #     # Opponent Player Speed
-    #     # -- Tutorial video has 2 players. Pickleball example is doubles so 4 players. (ID 1-2 close side, ID 3-4(weird issue ID skips 3 so 4-5 currently) far side)
-    #     opponent_player_id = 1 if player_shot_ball == 2 else 2
-    #     distance_covered_by_opponent_pixels = measure_distance(player_mini_court_detections[start_frame][opponent_player_id],
-    #                                                            player_mini_court_detections[end_frame][opponent_player_id])
-    #     distance_covered_by_opponent_meters = convert_pixel_distance_to_meters(distance_covered_by_opponent_pixels,
-    #                                                                        constants.COURT_WIDTH,
-    #                                                                        mini_court.get_width_of_mini_court()
-    #                                                                        )
-    #     speed_of_opponent = distance_covered_by_opponent_meters / ball_shot_time_in_seconds * 3.6
+        # Opponent Player Speed
+        # -- Tutorial video has 2 players. Pickleball example is doubles so 4 players. (ID 1-2 close side, ID 3-4(weird issue ID skips 3 so 4-5 currently) far side)
+        opponent_player_id = 1 if player_shot_ball == 2 else 2
+        distance_covered_by_opponent_pixels = measure_distance(player_mini_court_detections[start_frame][opponent_player_id],
+                                                               player_mini_court_detections[end_frame][opponent_player_id])
+        distance_covered_by_opponent_meters = convert_pixel_distance_to_meters(distance_covered_by_opponent_pixels,
+                                                                           constants.COURT_WIDTH,
+                                                                           mini_court.get_width_of_mini_court()
+                                                                           )
+        speed_of_opponent = distance_covered_by_opponent_meters / ball_shot_time_in_seconds * 3.6
 
-    #     # -- Deepcopy stats onto players
-    #     current_player_stats = deepcopy(player_stats_data[-1])
-    #     current_player_stats['frame_num'] = start_frame
-    #     current_player_stats[f'player_{player_shot_ball}_number_of_shots'] += 1
-    #     current_player_stats[f'player_{player_shot_ball}_total_shot_speed'] += speed_of_ball_shot
-    #     current_player_stats[f'player_{player_shot_ball}_last_shot_speed'] = speed_of_ball_shot
+        # -- Deepcopy stats onto players
+        current_player_stats = deepcopy(player_stats_data[-1])
+        current_player_stats['frame_num'] = start_frame
+        current_player_stats[f'player_{player_shot_ball}_number_of_shots'] += 1
+        current_player_stats[f'player_{player_shot_ball}_total_shot_speed'] += speed_of_ball_shot
+        current_player_stats[f'player_{player_shot_ball}_last_shot_speed'] = speed_of_ball_shot
 
-    #     current_player_stats[f'player_{player_shot_ball}_total_player_speed'] += speed_of_opponent
-    #     current_player_stats[f'player_{player_shot_ball}_last_player_speed'] = speed_of_opponent
+        current_player_stats[f'player_{player_shot_ball}_total_player_speed'] += speed_of_opponent
+        current_player_stats[f'player_{player_shot_ball}_last_player_speed'] = speed_of_opponent
 
-    #     player_stats_data.append(current_player_stats)
+        player_stats_data.append(current_player_stats)
 
-    # player_stats_data_df = pd.DataFrame(player_stats_data)
-    # frames_df = pd.DataFrame({'frame_num': list(range(len(video_frames)))})
-    # player_stats_data_df = pd.merge(frames_df, player_stats_data_df, on = 'frame_num', how = 'left')
-    # player_stats_data_df = player_stats_data_df.ffill()
-    # # Avg shot speed of players
-    # player_stats_data_df['player_1_average_shot_speed'] = player_stats_data_df['player_1_total_shot_speed'] / player_stats_data_df['player_1_number_of_shots']
-    # player_stats_data_df['player_2_average_shot_speed'] = player_stats_data_df['player_2_total_shot_speed'] / player_stats_data_df['player_2_number_of_shots']
+    player_stats_data_df = pd.DataFrame(player_stats_data)
+    frames_df = pd.DataFrame({'frame_num': list(range(len(video_frames)))})
+    player_stats_data_df = pd.merge(frames_df, player_stats_data_df, on = 'frame_num', how = 'left')
+    player_stats_data_df = player_stats_data_df.ffill()
+   
+    # Avg shot speed of players
+    player_stats_data_df['player_1_average_shot_speed'] = player_stats_data_df['player_1_total_shot_speed'] / player_stats_data_df['player_1_number_of_shots']
+    player_stats_data_df['player_2_average_shot_speed'] = player_stats_data_df['player_2_total_shot_speed'] / player_stats_data_df['player_2_number_of_shots']
     # player_stats_data_df['player_3_average_shot_speed'] = player_stats_data_df['player_3_total_shot_speed'] / player_stats_data_df['player_3_number_of_shots']
     # player_stats_data_df['player_4_average_shot_speed'] = player_stats_data_df['player_4_total_shot_speed'] / player_stats_data_df['player_4_number_of_shots']
-    # # Avg player speed
-    # player_stats_data_df['player_1_average_player_speed'] = player_stats_data_df['player_1_total_player_speed'] / player_stats_data_df['player_2_number_of_shots']
-    # player_stats_data_df['player_2_average_player_speed'] = player_stats_data_df['player_2_total_player_speed'] / player_stats_data_df['player_1_number_of_shots']
+    
+    # Avg player speed
+    player_stats_data_df['player_1_average_player_speed'] = player_stats_data_df['player_1_total_player_speed'] / player_stats_data_df['player_2_number_of_shots']
+    player_stats_data_df['player_2_average_player_speed'] = player_stats_data_df['player_2_total_player_speed'] / player_stats_data_df['player_1_number_of_shots']
     # player_stats_data_df['player_3_average_player_speed'] = player_stats_data_df['player_3_total_player_speed'] / player_stats_data_df['player_4_number_of_shots']
     # player_stats_data_df['player_4_average_player_speed'] = player_stats_data_df['player_4_total_player_speed'] / player_stats_data_df['player_3_number_of_shots']
 
